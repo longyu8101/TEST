1d9c1411133fb0221628c04184761ef8b59edccc
longyu8101 <1578494004@qq.com>
2023-01-12 18:17:30 +0800

    update0112

--- source/conf.py
+++ source/conf.py
@@ -18,6 +18,10 @@ language ='zh'
 
 templates_path = ['_templates']
 html_static_path = ['_static']
+html_css_files = [
+    'css/custom.css',
+]
+html_style = 'css/custom.css'
 exclude_patterns = []
 
 
